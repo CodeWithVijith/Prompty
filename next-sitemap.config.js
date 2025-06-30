b69f5a3461443801212276fa0ff44be14e2fde79
Vijith Shekha <developerndtester@gmail.com>
2025-06-30 20:18:15 +0530

    site map added

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  //   siteUrl: "https://prompty.in/",
+  siteUrl: "https://prompty-chi.vercel.app/",
+  generateRobotsTxt: true, // optional
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+};
